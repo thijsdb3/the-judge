8c3b6b31f47e93043c634c14bac151f28be6de79
thijsdb3 <thijsdebock28@gmail.com>
2024-10-31 20:57:19 +0100

    adding sign up to dev

--- judge/judge/src/app/(auth)/signup/page.jsx
+++ judge/judge/src/app/(auth)/signup/page.jsx
@@ -1,14 +1,14 @@
-// import styles from "./signup.module.css"
-// import RegisterForm from "@/components/auth/registerform/RegisterForm"
+import styles from "./signup.module.css"
+import RegisterForm from "@/components/auth/registerform/RegisterForm"
 
-// const SignUpPage = () => {
-//     return (
-//         <div className = {styles.container}>
-//         <div className = {styles.wrapper} >
-//              <RegisterForm/> 
-//         </div> 
-//         </div>
-//     )
-// }
+const SignUpPage = () => {
+    return (
+        <div className = {styles.container}>
+        <div className = {styles.wrapper} >
+             <RegisterForm/> 
+        </div> 
+        </div>
+    )
+}
 
-// export default SignUpPage
+export default SignUpPage
