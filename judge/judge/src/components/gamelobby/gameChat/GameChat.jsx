79ab02d1f0dfa0da61f544fa935ac14a0c3a860a
thijsdb3 <69469687+thijsdb3@users.noreply.github.com>
2024-11-13 21:59:55 +0100

    fix imports

--- judge/judge/src/components/gamelobby/gameChat/GameChat.jsx
+++ judge/judge/src/components/gamelobby/gameChat/GameChat.jsx
@@ -1,25 +1,11 @@
-
 import styles from "./gamechat.module.css";
 
 const Gamechat = () => {
   const gameMessages = ["Waiting on players...","game can be started once there are between 6 and 13 players in lobby","If you want to be the Judge click the volunteer button"];
 
   return (
-    <div className={styles.chatWindow}>
-      <div className={styles.navbar}>
-        <button className={styles.tab}>Game Messages</button>
-      </div>
-      <div className={styles.messages}>
-        <ul>
-          {gameMessages.map((message, index) => (
-            <li key={index} className={styles.message}>
-              {message}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    <div>test</div>
+  )
 };
 
 export default Gamechat;
