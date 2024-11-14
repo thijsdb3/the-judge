0e0f3e19baf68a8a466f785c17ec55c6b323e28f
thijsdb3 <69469687+thijsdb3@users.noreply.github.com>
2024-11-14 11:28:01 +0100

    fix imports

--- judge/judge/src/app/gamelobby/[id]/page.jsx
+++ judge/judge/src/app/gamelobby/[id]/page.jsx
@@ -1,6 +1,6 @@
 import Deck from "@/components/gamelobby/deck/Deck";
 import BoardState from "@/components/gamelobby/boardState/BoardState"
-import Gamechat from "@/components/gamelobby/gamechat/Gamechat";
+import GameChat from "@/components/gamelobby/gameChat/GameChat";
 import PlayerList from "@/components/gamelobby/players/PlayerList";
 import styles from "./page.module.css"
 import { auth } from "@/lib/auth";
@@ -23,7 +23,7 @@ const GamePage = async (context) => {
               <BoardState />
             </div>
             <div className={styles.section}>
-              <Gamechat/>
+              <GameChat/>
             </div>
           </div>
         </div>
--- judge/judge/src/components/gamelobby/gameChat/GameChat.jsx
+++ judge/judge/src/components/gamelobby/gameChat/GameChat.jsx
@@ -1,7 +1,25 @@
-//import styles from "./gamechat.module.css";
 
-const Gamechat = () => {
- return <div>hi</div>
+import styles from "./gamechat.module.css";
+
+const GameChat = () => {
+  const gameMessages = ["Waiting on players...","game can be started once there are between 6 and 13 players in lobby","If you want to be the Judge click the volunteer button"];
+
+  return (
+    <div className={styles.chatWindow}>
+      <div className={styles.navbar}>
+        <button className={styles.tab}>Game Messages</button>
+      </div>
+      <div className={styles.messages}>
+        <ul>
+          {gameMessages.map((message, index) => (
+            <li key={index} className={styles.message}>
+              {message}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-export default Gamechat;
+export default GameChat;
