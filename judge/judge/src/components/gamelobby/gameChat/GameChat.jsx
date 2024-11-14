dad3137d2ac54cb7b70e93c39b9be5a60b7638c6
thijsdb3 <69469687+thijsdb3@users.noreply.github.com>
2024-11-14 11:23:01 +0100

    fix imports

--- judge/judge/src/components/gamelobby/gameChat/GameChat.jsx
+++ judge/judge/src/components/gamelobby/gameChat/GameChat.jsx
@@ -1,4 +1,4 @@
-import styles from "./gamechat.module.css";
+//import styles from "./gamechat.module.css";
 
 const Gamechat = () => {
   const gameMessages = ["Waiting on players...","game can be started once there are between 6 and 13 players in lobby","If you want to be the Judge click the volunteer button"];
