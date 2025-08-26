ce6c1dc17cbb29ae3ad7de6e2d400de9b23d9158
thijsdb3 <69469687+thijsdb3@users.noreply.github.com>
2025-08-26 15:45:26 -0700

    .

--- judge/judge/src/app/api/game/playerlist/route.js
+++ judge/judge/src/app/api/game/playerlist/route.js
@@ -4,18 +4,16 @@ import { Game } from "@/lib/models";
 
 export async function POST(req) {
   try {
-    const { lobbyid } = await req.json(); // Destructure lobbyid from request body
-    await connectToDB(); // Connect to the database
+    const { lobbyid } = await req.json(); 
+    await connectToDB(); 
 
     const game = await Game.findOne({ gameid: lobbyid }).populate(
       "players.player"
-    ); // Populate player details from User model
-
+    ); 
     if (!game) {
       return NextResponse.json({ error: "Game not found" }, { status: 404 });
     }
 
-    // Extract usernames from the populated players
     const playerData = game.players.map((p) => ({
       username: p.player.username,
       role: p.role,
--- judge/judge/src/app/page.jsx
+++ judge/judge/src/app/page.jsx
@@ -7,6 +7,7 @@ const HomePage = () => {
   const router = useRouter();
 
   return (
+    <div className = {styles.all}>
     <div className={styles.container}>
         <h1 className={styles.title}>The Judge</h1>
 
@@ -36,6 +37,7 @@ const HomePage = () => {
             Join Game
           </button>
         </div>
+      </div>
       </div>
   );
 };
